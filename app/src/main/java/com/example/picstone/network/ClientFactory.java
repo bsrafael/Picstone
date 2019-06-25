eac8f0bab58a489bf6bd9ff12a1868f60c90df15
Julio De Bastiani <julioc.debastiani@gmail.com>
2019-06-25 00:47:15 -0300

    Authentication

--- app/src/main/java/com/example/picstone/network/ClientFactory.java
+++ app/src/main/java/com/example/picstone/network/ClientFactory.java
@@ -1,5 +1,11 @@
 package com.example.picstone.network;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -7,8 +13,29 @@ public class ClientFactory {
     // TODO alterar para a url real depois de hosteado
     public static final String BASE_URL = "http://localhost:5000";
 
-    public static final SoapstoneClient GetSoapstoneClient() {
+    public static final SoapstoneClientPublic GetSoapstoneClient() {
         Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        return retrofit.create(SoapstoneClientPublic.class);
+    }
+
+    public static final SoapstoneClient GetSoapstoneClient(final String token) {
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request newRequest = chain.request().newBuilder()
+                        .addHeader("Authorization", "Bearer " + token)
+                        .build();
+
+                return chain.proceed(newRequest);
+            }
+        }).build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .client(client)
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
--- app/src/main/java/com/example/picstone/network/SoapstoneClient.java
+++ app/src/main/java/com/example/picstone/network/SoapstoneClient.java
@@ -17,86 +17,77 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
-import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Part;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
-// TODO tirar parametro do header e ver como criar um interceptor
-// TODO provavelmente ficaria mais organizado separar em 3 classes
 public interface SoapstoneClient {
     // Tokens
-    @POST("api/tokens")
-    Call<TokenViewModel> getToken(@Body TokenInputModel inputModel);
-
     @GET("api/tokens")
-    Call authenticate(@Header("Authorization") String token);
+    Call authenticate();
 
     // Users
     @GET("api/users")
-    Call<List<UserViewModel>> getUsers(@Header("Authorization") String token);
+    Call<List<UserViewModel>> getUsers();
 
     @GET("api/users")
-    Call<List<UserViewModel>> getUsers(@Header("Authorization") String token, @Query("skip") int skip, @Query("take") int take);
+    Call<List<UserViewModel>> getUsers(@Query("skip") int skip, @Query("take") int take);
 
     @GET("api/users/{id}")
-    Call<UserViewModel> getUser(@Header("Authorization") String token, @Path(value = "id") String id);
+    Call<UserViewModel> getUser(@Path(value = "id") String id);
 
     @GET("api/users/{id}/posts")
-    Call<List<PostViewModel>> getUserPosts(@Header("Authorization") String token, @Path(value = "id") String id);
+    Call<List<PostViewModel>> getUserPosts(@Path(value = "id") String id);
 
     @GET("api/users/{id}/posts")
-    Call<List<PostViewModel>> getUserPosts(@Header("Authorization") String token, @Path(value = "id") String id, @Query("skip") int skip, @Query("take") int take);
+    Call<List<PostViewModel>> getUserPosts(@Path(value = "id") String id, @Query("skip") int skip, @Query("take") int take);
 
     @GET("api/users/{id}/saved")
-    Call<List<PostViewModel>> getUserSavedPosts(@Header("Authorization") String token, @Path(value = "id") String id);
+    Call<List<PostViewModel>> getUserSavedPosts(@Path(value = "id") String id);
 
     @GET("api/users/{id}/saved")
-    Call<List<PostViewModel>> getUserSavedPosts(@Header("Authorization") String token, @Path(value = "id") String id, @Query("skip") int skip, @Query("take") int take);
-
-    @POST("api/users")
-    Call<UserViewModel> createUser(@Body UserInputModel inputModel);
+    Call<List<PostViewModel>> getUserSavedPosts(@Path(value = "id") String id, @Query("skip") int skip, @Query("take") int take);
 
     @PUT("api/users/{id}")
-    Call changePassword(@Header("Authorization") String token, @Path(value = "id") String id, @Body ChangePasswordInputModel inputModel);
+    Call changePassword(@Path(value = "id") String id, @Body ChangePasswordInputModel inputModel);
 
     @DELETE("api/users/{id}")
-    Call deleteUser(@Header("Authorization") String token, @Path(value = "id") String id);
+    Call deleteUser(@Path(value = "id") String id);
 
     // Posts
     @GET("api/posts")
-    Call<List<PostViewModel>> getPostsNearUser(@Header("Authorization") String token, @Query("latitude") double latitude, @Query("longitude") double longitude);
+    Call<List<PostViewModel>> getPostsNearUser(@Query("latitude") double latitude, @Query("longitude") double longitude);
 
     @GET("api/posts")
-    Call<List<PostViewModel>> getPostsNearUser(@Header("Authorization") String token, @Query("latitude") double latitude, @Query("longitude") double longitude, @Query("skip") int skip, @Query("take") int take);
+    Call<List<PostViewModel>> getPostsNearUser(@Query("latitude") double latitude, @Query("longitude") double longitude, @Query("skip") int skip, @Query("take") int take);
 
     @GET("api/posts/top")
-    Call<List<PostViewModel>> getTopPosts(@Header("Authorization") String token);
+    Call<List<PostViewModel>> getTopPosts();
 
     @GET("api/posts/top")
-    Call<List<PostViewModel>> getTopPosts(@Header("Authorization") String token, @Query("skip") int skip, @Query("take") int take);
+    Call<List<PostViewModel>> getTopPosts(@Query("skip") int skip, @Query("take") int take);
 
     @POST("api/posts/image") // TODO essa rota provavelmente vai mudar
-    Call<ImageViewModel> uploadImage(@Header("Authorization") String token, @Part MultipartBody.Part image, @Part("name") RequestBody requestBody);
+    Call<ImageViewModel> uploadImage(@Part MultipartBody.Part image, @Part("name") RequestBody requestBody);
     // TODO finalizar: https://stackoverflow.com/questions/39953457/how-to-upload-image-file-in-retrofit-2
 
     @POST("api/posts")
-    Call<PostViewModel> createPost(@Header("Authorization") String token, @Body PostInputModel inputModel);
+    Call<PostViewModel> createPost(@Body PostInputModel inputModel);
 
     @DELETE("api/posts/{id}")
-    Call deletePost(@Header("Authorization") String token, @Path(value = "id") String id);
+    Call deletePost(@Path(value = "id") String id);
 
     @PUT("api/posts/{id}/upvote")
-    Call upvotePost(@Header("Authorization") String token, @Path(value = "id") String id);
+    Call upvotePost(@Path(value = "id") String id);
 
     @PUT("api/posts/{id}/downvote")
-    Call downvotePost(@Header("Authorization") String token, @Path(value = "id") String id);
+    Call downvotePost(@Path(value = "id") String id);
 
     @PUT("api/posts/{id}/save")
-    Call savePost(@Header("Authorization") String token, @Path(value = "id") String id);
+    Call savePost(@Path(value = "id") String id);
 
     @PUT("api/posts/{id}/report")
-    Call reportPost(@Header("Authorization") String token, @Path(value = "id") String id);
+    Call reportPost(@Path(value = "id") String id);
 }
