268b2d450f71e2ee62a7a2f55840f7cf3daec444
Julio De Bastiani <julioc.debastiani@gmail.com>
2019-07-02 20:43:37 -0300

    Tentativa de acertar o login

--- app/src/main/java/com/example/picstone/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/picstone/ui/login/LoginActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
@@ -31,41 +32,45 @@ public class LoginActivity extends AppCompatActivity {
 
     private LoginViewModel loginViewModel;
 
+    private class LoginTask extends AsyncTask<Void, Void, LoggedInUser> {
+        @Override
+        protected LoggedInUser doInBackground(Void... v) {
+            // NOTE nao sei se isso vai funcionar
+            // se nao funcionar fazer com uma call mesmo
+            final EditText usernameEditText = findViewById(R.id.username);
+            final EditText passwordEditText = findViewById(R.id.password);
+            final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+
+            loadingProgressBar.setVisibility(View.VISIBLE);
+
+            LoginRepository loginRepository = LoginRepository.getInstance(new LoginDataSource());
+            Result<LoggedInUser> result = loginRepository.login(usernameEditText.getText().toString(), passwordEditText.getText().toString());
+
+            if (result instanceof Result.Success) {
+                LoggedInUser data = ((Result.Success<LoggedInUser>) result).getData();
+                updateUiWithUser();
+                return data;
+            } else {
+                // TODO mensagem de que o login falhou
+                return null;
+            }
+        }
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        final EditText usernameEditText = findViewById(R.id.username);
-        final EditText passwordEditText = findViewById(R.id.password);
         final Button loginButton = findViewById(R.id.loginBtn);
         final Button registerButton = findViewById(R.id.create_account);
-        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
-        final boolean DEBUG_BYPASS_LOGIN = true;
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadingProgressBar.setVisibility(View.VISIBLE);
-//                System.out.println("\n---deu click (LoginActivity:49)");
-//
-//                LoginRepository loginRepository = LoginRepository.getInstance(new LoginDataSource());
-//                Result<LoggedInUser> result = loginRepository.login(usernameEditText.getText().toString(), passwordEditText.getText().toString());
-//                System.out.println("\n---vai pro if (LoginActivity:53)");
-//
-//                if (result instanceof Result.Success) {
-//                    LoggedInUser data = ((Result.Success<LoggedInUser>) result).getData();
-//                    updateUiWithUser();
-//                    System.out.println("\n----deu certo (LoginActivity:58)");
-//
-//                } else {
-//                    // TODO a message or something
-//                    System.out.println("\n----deu pau (LoginActivity:62)");
-//                }
-                if (DEBUG_BYPASS_LOGIN) updateUiWithUser(); //TODO: remove this after login is fixed.
+                new LoginTask().execute();
             }
         });
-
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,7 +83,6 @@ public class LoginActivity extends AppCompatActivity {
 
     private void updateUiWithUser() {
         String welcome = getString(R.string.welcome);
-        // TODO : initiate successful logged in experience
         Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
