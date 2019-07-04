aa60161af8a884c9a18cdd49832595f6253a360c
Rafael <rafael.silveira@duo.studio>
2019-07-03 23:17:45 -0300

    minor fixes

--- app/src/main/java/com/example/picstone/main/CreatePostActivity.java
+++ app/src/main/java/com/example/picstone/main/CreatePostActivity.java
@@ -92,6 +92,8 @@ public class CreatePostActivity extends AppCompatActivity {
                 if (!validate())
                     return;
 
+                uploadedUrl = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/bc8f83c5-2e9c-472f-9146-37d230ff5d99/d5wdr3p-3931de90-4cfb-4d83-bbd1-0132cea3f296.jpg/v1/fill/w_774,h_1033,q_70,strp/orange_guidance_soapstone_by_hellduck_d5wdr3p-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTM2NiIsInBhdGgiOiJcL2ZcL2JjOGY4M2M1LTJlOWMtNDcyZi05MTQ2LTM3ZDIzMGZmNWQ5OVwvZDV3ZHIzcC0zOTMxZGU5MC00Y2ZiLTRkODMtYmJkMS0wMTMyY2VhM2YyOTYuanBnIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.yiXdHr2s87R_GR40JElaJKqIII0lmmWF-npppsQg4Gw";
+
                 if (uploadedUrl == null)
                     UploadImage();
                 else
@@ -111,15 +113,20 @@ public class CreatePostActivity extends AppCompatActivity {
 
     private void UploadImage() {
         File file = new File(imageUri.getPath());
-        // RequestBody requestFile = RequestBody.create(MediaType.parse("multipart/form-data"), file);
-        RequestBody requestFile = RequestBody.create(MediaType.parse("image/*"), file);
+         RequestBody requestFile = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+//        RequestBody requestFile = RequestBody.create(MediaType.parse("image/*"), file);
         MultipartBody.Part body = MultipartBody.Part.createFormData("image", file.getName(), requestFile);
 
         client.uploadImage(body).enqueue(new Callback<ImageViewModel>() {
             @Override
             public void onResponse(Call<ImageViewModel> call, Response<ImageViewModel> response) {
-                uploadedUrl = response.body().getImageUrl();
-                CreatePost();
+                if (response.isSuccessful()) {
+                    uploadedUrl = response.body().getImageUrl();
+                    CreatePost();
+                } else {
+                    Toast.makeText(getApplicationContext(), response.message(), Toast.LENGTH_LONG);
+                    System.out.println("\n\n\n" + response.message());
+                }
             }
 
             @Override
@@ -139,10 +146,11 @@ public class CreatePostActivity extends AppCompatActivity {
         client.createPost(inputModel).enqueue(new Callback<PostViewModel>() {
             @Override
             public void onResponse(Call<PostViewModel> call, Response<PostViewModel> response) {
-                if (true) {
+                if (response.isSuccessful()) {
                     setResult(Activity.RESULT_OK);
                 }
 
+                System.out.println(response.message());
                 finish();
             }
 
